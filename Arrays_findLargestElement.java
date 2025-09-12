0f4067c117cac6df9bf359eab09bbd8737cfeb0c
Raju Nishad <161194437+nishadraju@users.noreply.github.com>
2025-09-12 16:05:28 +0530

    Add files via upload

---
+++ Arrays_findLargestElement.java
@@ -0,0 +1,23 @@
+package p6;
+
+public class Arrays_findLargestElement {
+	public static void main(String[] args) {
+
+		int a[] = { 10, 2, 3, 4, 5, 6 };
+
+		int max = a[0];
+
+		for (int i = 0; i < a.length; i++) {
+
+			if (max < a[i]) {
+				max = a[i];
+
+			}
+
+		}
+		
+		System.out.println(max);
+
+	}
+
+}
