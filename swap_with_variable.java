08f91c443bc790cb5bd7c480e12ff307af0ddf9d
Raju Nishad <161194437+nishadraju@users.noreply.github.com>
2025-09-12 16:04:19 +0530

    Add files via upload

---
+++ swap_with_variable.java
@@ -0,0 +1,22 @@
+
+public class swap_with_variable {
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int t;
+		System.out.println(a);
+		System.out.println(b);
+		
+		t=a;
+		a=b;
+		b=t;
+		
+		System.out.println("-------------------------------------------");
+		System.out.println(a);
+        System.out.println(b);		
+		
+		
+	}
+
+}
